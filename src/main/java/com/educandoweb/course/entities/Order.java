080f89d15b458108ab15d44ca6a36403a1de139b
luiseduardo85 <luiseduardo318@gmail.com>
2025-03-28 21:33:13 -0300

    Feat: entidade pedido

---
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -0,0 +1,66 @@
+package com.educandoweb.course.entities;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_order")
+public class Order implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Instant moment;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private TbUser client;
+
+    public Order() {
+    }
+
+    public Order(Long id, Instant moment, TbUser client) {
+        this.id = id;
+        this.moment = moment;
+        this.client = client;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public TbUser getClient() {
+        return client;
+    }
+
+    public void setClient(TbUser client) {
+        this.client = client;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Order order)) return false;
+        return Objects.equals(id, order.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
--- src/main/java/com/educandoweb/course/entities/TbUser.java
+++ src/main/java/com/educandoweb/course/entities/TbUser.java
@@ -4,6 +4,8 @@ package com.educandoweb.course.entities;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -20,6 +22,9 @@ public class TbUser implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public TbUser() {
     }
@@ -81,5 +86,9 @@ public class TbUser implements Serializable {
     @Override
     public int hashCode() {
         return Objects.hashCode(id);
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 }
