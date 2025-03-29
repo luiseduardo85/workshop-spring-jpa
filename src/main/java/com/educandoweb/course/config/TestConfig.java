fd17928685536e599874ff4b132ee3eb8a0f47ff
luiseduardo85 <luiseduardo318@gmail.com>
2025-03-29 05:39:39 -0300

    Feat: order repository
    Add orders and instant

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -1,12 +1,15 @@
 package com.educandoweb.course.config;
 
+import com.educandoweb.course.entities.Order;
 import com.educandoweb.course.entities.TbUser;
+import com.educandoweb.course.repositories.OrderRepository;
 import com.educandoweb.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 
@@ -17,13 +20,22 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private OrderRepository orderRepository;
+
     @Override
     public void run(String... args) throws Exception {
 
         TbUser u1 = new TbUser(null, "Maria", "maria@gmail.com", "999999999", "123456");
         TbUser u2 = new TbUser(null, "João", "joao@gmail.com", "987654321", "123456");
 
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+
         userRepository.saveAll(Arrays.asList(u1,u2));
+
+        orderRepository.saveAll(Arrays.asList(o1, o2, o3));
     }
 
 }
--- src/main/java/com/educandoweb/course/entities/TbUser.java
+++ src/main/java/com/educandoweb/course/entities/TbUser.java
@@ -1,6 +1,7 @@
 package com.educandoweb.course.entities;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -23,6 +24,7 @@ public class TbUser implements Serializable {
     private String phone;
     private String password;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "client")
     private List<Order> orders = new ArrayList<>();
 
